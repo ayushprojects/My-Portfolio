151ec71c05b82224ee756f3a97f96b7acfab8aa0
Ayush namdev <ayushnamdev375@gmail.com>
2023-03-19 02:29:45 +0530

    commit

---
+++ index.js
@@ -0,0 +1,11 @@
+function goToProjectPage() {
+  window.location.href = "projects.html";
+}
+
+function goToMainPage() {
+  window.location.href = "index.html";
+}
+
+function goToCvPage() {
+  window.location.href = "cv.html";
+}
